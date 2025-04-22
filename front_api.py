07e0a2788e3686400237e3e9eb20e9ff9f23676e
Johhan Ramirez <jsra65@hotmail.com>
2025-04-22 11:51:58 -0500

    Update front_api.py

--- front_api.py
+++ front_api.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://api.langflow.astra.datastax.com/lf/741d247a-5163-49d3-9175-9450dff46b93/api/v1/run/0791cbe2-437c-4d5d-9df0-23cbad666ca0"  # Replace with your API URL
+# API_URL = "https://api.langflow.astra.datastax.com/lf/741d247a-5163-49d3-9175-9450dff46b93/api/v1/run/0791cbe2-437c-4d5d-9df0-23cbad666ca0"  # Replace with your API URL
 
 col1, col2 = st.columns([3,1])
 
@@ -29,6 +29,11 @@ st.markdown("""
     box-shadow: 2px 2px 5px rgba(0,255,0,0.75); /* Green shadow when hovered */
 }
 
+    .stButton button:focus {
+        border: 2px solid #7cdb91; /* Green border color when focused */
+        color: #7cdb91; /* Green text color when focused */
+    }
+
     /* CSS for changing the border color and shadow of the text input box */
     .stTextInput>div>div>input {
         border: 2px solid #7C01FF; /* Blue border color */
@@ -48,15 +53,37 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
+def connect_api(query):
+    BASE_API_URL = "https://api.langflow.astra.datastax.com"
+    LANGFLOW_ID = "40e98986-3486-471e-8c5d-6a27d01f3d78"
+    FLOW_ID = "a4bb8b81-8469-4cd6-8898-d31597bed217"
+    APPLICATION_TOKEN  = "AstraCS:UByPvhxWgxaObuiRdHYkwNMl:cc45f1eeb9a640236415222c6d65eeef6937fca25f6276653ec8fdd9b324b16d"
+    headers = {
+        "Authorization": f"Bearer {APPLICATION_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "input_value": query,
+        "output_type": "chat",
+        "input_type": "chat"
+    }
+
+    responses = requests.post(f"{BASE_API_URL}/lf/{LANGFLOW_ID}/api/v1/run/{FLOW_ID}", json=payload, headers=headers, timeout=90) 
+    return responses
+    
+
 # Usa st.form para agrupar el input y botón
 with st.form(key='chat_form'):
     query = st.text_input("Realiza las preguntas que hacen tus brokers:")
     submit_button = st.form_submit_button(label='Responder')
 
 if submit_button and query:
-    response = requests.get(API_URL, params={"query": query})
-
-    if response.status_code == 200:
-        st.write("🔍 Respuesta:", response.json().get("response", "⚠️ No 'response' key found in JSON."))
+    response_api = connect_api(query)
+    
+    if response_api.status_code != 200:
+        st.write("⚠️ No 'response' key found in JSON.", response_api)
     else:
-        st.error(f"❌ API Error: {response.status_code}")
+        data = response_api.json()
+        data = data["outputs"][0]["outputs"][0]["results"]["message"]["text"]
+        st.write(data)
