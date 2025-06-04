e14f553ebd7a47977a398c655b2932d0d9560643
Johhan Ramirez <jsra65@hotmail.com>
2025-06-04 14:35:52 -0500

    Update front_api.py

--- front_api.py
+++ front_api.py
@@ -75,7 +75,10 @@ def connect_api(query):
     
 with st.expander("¿Cómo realizar una preguntar?"):
     st.markdown("""
-    Para hacer una pregunta correcta al agente usa la siguiente estructura\nPara obtener la mejor respuesta sobre políticas y reglas de crédito, estructura tu pregunta indicando claramente la acción que deseas que realice nuestro Agente de Habicredit (ej. "explica", "enumera", "dime"), el tema específico (ej. "requisitos para radicar", "políticas de desembolso", "documentos necesarios") y cualquier condición o detalle adicional \n(ej. "para crédito hipotecario", "si ya tengo un crédito", "paso a paso"). \nSé lo más directo y específico posible para una respuesta precisa.
+    Para hacer una pregunta correcta al agente usa la siguiente estructura
+    \nPara obtener la mejor respuesta sobre políticas y reglas de crédito, estructura tu pregunta indicando claramente la acción que deseas que realice nuestro Agente de Habicredit (ej. "explica", "enumera", "dime"), 
+    \nel tema específico (ej. "requisitos para radicar", "políticas de desembolso", "documentos necesarios") y cualquier condición o detalle adicional 
+    \n(ej. "para crédito hipotecario", "si ya tengo un crédito", "paso a paso"). \nSé lo más directo y específico posible para una respuesta precisa.
     \n\nPor último, mo olvides calificar la respuesta obtenida.
     """)
     
